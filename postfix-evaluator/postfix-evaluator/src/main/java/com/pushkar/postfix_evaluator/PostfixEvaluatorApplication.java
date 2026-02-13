9b391784a270b8bf6d720c5b7ab3a493529e601b
Pushkkaarr <pushkarmmhatre3090@gmail.com>
2026-02-13 11:27:34 +0530

    first commit

---
+++ postfix-evaluator/postfix-evaluator/src/main/java/com/pushkar/postfix_evaluator/PostfixEvaluatorApplication.java
@@ -0,0 +1,13 @@
+package com.pushkar.postfix_evaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostfixEvaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostfixEvaluatorApplication.class, args);
+	}
+
+}
---
+++ postfix-evaluator/postfix-evaluator/src/test/java/com/pushkar/postfix_evaluator/PostfixEvaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pushkar.postfix_evaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostfixEvaluatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
